615bceeaa038e84e4dd12100caa36168954fb57d
AnzoTheGamer <antonr@live.dk>
2016-11-15 15:27:06 +0100

    Fixed destrying blocks while Craftingmenu is open

--- Assets/Scripts/BlockScript.cs
+++ Assets/Scripts/BlockScript.cs
@@ -2,37 +2,52 @@
 using System.Collections;
 
 public class BlockScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public Canvas CraftingMenu;
+    private bool DestryBlocks;
+    // Use this for initialization
+    void Start() {
 
     }
 
     // Update is called once per frame
-    void Update () {
-
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            if (CraftingMenu.enabled == true)
+            {
+                DestryBlocks = false;
+            }
+            else
+            {
+                DestryBlocks = true;
+            }
+        }
     }
 
 
-    void  OnMouseOver()
+        void OnMouseOver()
     {
-        if (Input.GetMouseButton(0))
-        {  // Mouse left-click: Remove a block
-            // Find "MakeWorld" script
-            GameObject mwo = GameObject.Find("LoadLevel");
-            MakeWorld mw = mwo.GetComponent<MakeWorld>();
-            // Find block index x,y at mouse
-            IntVector2 bxy = mw.GetBlockIndexAtMouse();
-            // Find distance to player
-            if (mw.DistanceBlockToPlayer(bxy)<4)
-            {
-                // Block is close to player
-                GameObject mb = GameObject.Find("MinedBlock");
-                MinedBlockScript mbs = mb.GetComponent<MinedBlockScript>();
-                mbs.MineBlock(bxy, gameObject);
-//                mw.RemoveBlock(gameObject);
+        if(DestryBlocks == true) {
+            if (Input.GetMouseButton(0))
+            {  // Mouse left-click: Remove a block
+               // Find "MakeWorld" script
+                GameObject mwo = GameObject.Find("LoadLevel");
+                MakeWorld mw = mwo.GetComponent<MakeWorld>();
+                // Find block index x,y at mouse
+                IntVector2 bxy = mw.GetBlockIndexAtMouse();
+                // Find distance to player
+                if (mw.DistanceBlockToPlayer(bxy) < 4)
+                {
+                    // Block is close to player
+                    GameObject mb = GameObject.Find("MinedBlock");
+                    MinedBlockScript mbs = mb.GetComponent<MinedBlockScript>();
+                    mbs.MineBlock(bxy, gameObject);
+                    //                mw.RemoveBlock(gameObject);
 
+                }
             }
+
+           }
         }
-   }
-}
+    }
