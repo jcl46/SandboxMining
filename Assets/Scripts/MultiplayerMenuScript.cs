2fcdd14db05878414bfc0720eb8353ac74b1f98d
AnzoTheGamer <antonr@live.dk>
2016-11-21 13:26:11 +0100

    Added Multiplayer Scene and Script

---
+++ Assets/Scripts/MultiplayerMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MultiplayerMenuScript : MonoBehaviour
+{
+    public Button Cancel;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    public void CancelCLick()
+
+    {
+    SceneManager.LoadScene(1);
+    }
+}
