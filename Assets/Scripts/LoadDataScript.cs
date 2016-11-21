e9d81b98f94f2e7fb5be9d5ae7e279dab81ca390
AnzoTheGamer <antonr@live.dk>
2016-11-21 14:01:46 +0100

    Saves worldName into Load World screen

---
+++ Assets/Scripts/LoadDataScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadDataScript : MonoBehaviour {
+    public Button World1;
+    public Text WorldNameText;
+	// Use this for initialization
+	void Start () {
+        World1 = World1.GetComponent<Button>();
+        WorldNameText = WorldNameText.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void LoadData(string newName)
+    {
+        WorldNameText.text = newName;
+    }
+}
---
+++ Assets/Scripts/WorldNameRememberScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WorldNameRememberScript : MonoBehaviour {
+    public InputField WorldNameInput;
+    public static string WorldName;
+
+    void Start()
+    {
+        if (WorldName != null)
+        {
+
+            WorldNameInput.text = WorldName;
+        }
+    }
+
+    public void SaveData(string newName)
+    {
+        WorldName = newName;
+    }
+// Update is called once per frame
+    void Update () {
+
+
+                     }
+   
+}
+
